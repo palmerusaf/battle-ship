6866f3ff939d0fed2aa83f9de3444089412858f1
Branden Palmer <brandenpalmer08@gmail.com>
2022-04-16 21:47:51 +0700

    add tests for ship coordinate methods

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -18,3 +18,27 @@ it("newShip.isSunk Test -- hit and sunk test", () => {
   expect(newShip.isSunk()).toBe(true);
   expect(newShip.getLength()).toBe(2);
 });
+
+it("Set ship location horizontally Test 1", () => {
+  const newShip = Ship(5);
+  newShip.setCoordinates(0, "x");
+  expect(newShip.getCoordinates()).toEqual([0, 1, 2, 3, 4]);
+});
+
+it("Set ship location horizontally Test 2", () => {
+  const newShip = Ship(4);
+  newShip.setCoordinates(10, "x");
+  expect(newShip.getCoordinates()).toEqual([10, 11, 12, 13]);
+});
+
+it("Set ship location vertically Test 1", () => {
+  const newShip = Ship(5);
+  newShip.setCoordinates(1, "y");
+  expect(newShip.getCoordinates()).toEqual([1,11,21,31,41]);
+});
+
+it("Set ship location vertically Test 2", () => {
+  const newShip = Ship(4);
+  newShip.setCoordinates(9, "y");
+  expect(newShip.getCoordinates()).toEqual([9,19,29,39]);
+});
