5aac3f69f17388979c587a43daae51b3fc4a205d
Branden Palmer <brandenpalmer08@gmail.com>
2022-05-13 16:03:01 +0700

    refactor grid css  Please enter the commit message for your changes. Lines starting

--- src/modules/ship-placement-screen.js
+++ src/modules/ship-placement-screen.js
@@ -31,7 +31,7 @@ export function shipPlacementScreen() {
 
   const placementGrid = Components.newGrid();
   placementGrid.setClickable();
-  placementGrid.addParentClass('placement-grid')
+  placementGrid.addParentClass("placement-grid");
   placementGrid.setCoordinateHoverMsg("Place Ship at");
   shipPlacementScreen.appendChild(placementGrid.render());
 
