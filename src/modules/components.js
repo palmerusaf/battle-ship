b3c83a519fa7eaa07c90552dffdcb95635c7e1ac
Branden Palmer <brandenpalmer08@gmail.com>
2022-05-14 16:18:28 +0700

    mv new grid to separate module

--- src/modules/components.js
+++ src/modules/components.js
@@ -1,4 +1,5 @@
 import "../styles/components.scss";
+import { newGrid } from "./new-grid";
 
 export const Components = (() => {
   function newButton(label) {
@@ -6,65 +7,6 @@ export const Components = (() => {
     button.classList.add("button", "clickable");
     button.textContent = label;
     return button;
-  }
-
-  function newGrid() {
-    const GRID_SIZE = 100;
-    const gridContainer = initGridWithElements();
-
-    function initGridWithElements() {
-      const gridContainer = document.createElement("div");
-      gridContainer.classList.add("grid-container");
-      for (let i = 0; i < GRID_SIZE; i++) {
-        const gridElement = document.createElement("div");
-        gridElement.classList.add("grid-container__element");
-        gridContainer.appendChild(gridElement);
-      }
-      return gridContainer;
-    }
-
-    function _modElements(mod) {
-      const gridElements = [...gridContainer.children];
-      gridElements.forEach(mod);
-    }
-
-    function render() {
-      return gridContainer;
-    }
-
-    function setClickable() {
-      _modElements((gridElement) => {
-        gridElement.setAttribute("tabindex", "0");
-        gridElement.classList.add("clickable");
-      });
-    }
-
-    function setCoordinateHoverMsg(message) {
-      _modElements(
-        (gridElement, index) =>
-          (gridElement.title = `${message} ${xYCoordinate(index)}`)
-      );
-
-      function xYCoordinate(index) {
-        return `(${(index % 10) + 1}, ${Math.floor(index / 10 + 1)})`;
-      }
-    }
-
-    function addParentClass(className) {
-      gridContainer.classList.add(className);
-    }
-
-    function addChildClass(className) {
-      _modElements((gridElement) => gridElement.classList.add(className));
-    }
-
-    return {
-      render,
-      setClickable,
-      setCoordinateHoverMsg,
-      addParentClass,
-      addChildClass,
-    };
   }
 
   return {
