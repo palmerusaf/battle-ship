ebc3c7766dfb53c4e551b2c46acc333e55c82629
Branden Palmer <brandenpalmer08@gmail.com>
2022-05-14 13:42:45 +0700

    refactor newGrid component

--- src/modules/components.js
+++ src/modules/components.js
@@ -7,44 +7,60 @@ export const Components = (() => {
     button.textContent = label;
     return button;
   }
+
   function newGrid() {
     const GRID_SIZE = 100;
-    const gridContainer = document.createElement("div");
-    gridContainer.classList.add("grid-container");
-    for (let i = 0; i < GRID_SIZE; i++) {
-      const gridElement = document.createElement("div");
-      gridElement.classList.add("grid-container__element");
-      gridContainer.appendChild(gridElement);
+    const gridContainer = initGridWithElements();
+
+    function initGridWithElements() {
+      const gridContainer = document.createElement("div");
+      gridContainer.classList.add("grid-container");
+      for (let i = 0; i < GRID_SIZE; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("grid-container__element");
+        gridContainer.appendChild(gridElement);
+      }
+      return gridContainer;
     }
+
     function _modElements(mod) {
       const gridElements = [...gridContainer.children];
       gridElements.forEach(mod);
     }
 
+    function render() {
+      return gridContainer;
+    }
+
+    function setClickable() {
+      _modElements((gridElement) => gridElement.setAttribute("tabindex", "0"));
+    }
+
+    function setCoordinateHoverMsg(message) {
+      _modElements(
+        (gridElement, index) =>
+          (gridElement.title = `${message} ${xYCoordinate(index)}`)
+      );
+
+      function xYCoordinate(index) {
+        return `(${(index % 10) + 1}, ${Math.floor(index / 10 + 1)})`;
+      }
+    }
+
+    function addParentClass(className) {
+      gridContainer.classList.add(className);
+    }
+
+    function addChildClass(className) {
+      _modElements((gridElement) => gridElement.classList.add(className));
+    }
+
     return {
-      render: () => {
-        return gridContainer;
-      },
-      
-      setClickable: () =>
-        _modElements((gridElement) =>
-          gridElement.setAttribute("tabindex", "0")
-        ),
-        
-      setCoordinateHoverMsg: (message) => {
-        _modElements(
-          (gridElement, index) =>
-            (gridElement.title = `${message} ${xYCoordinate(index)}`)
-        );
-        function xYCoordinate(index) {
-          return `(${(index % 10) + 1}, ${Math.floor(index / 10 + 1)})`;
-        }
-      },
-      
-      addParentClass: (className) => gridContainer.classList.add(className),
-      
-      addChildClass: (className) =>
-        _modElements((gridElement) => gridElement.classList.add(className)),
+      render,
+      setClickable,
+      setCoordinateHoverMsg,
+      addParentClass,
+      addChildClass,
     };
   }
 
